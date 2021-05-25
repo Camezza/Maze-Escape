6f48b4aa333b1e7a935571aa4f525ca7f240178f
Firejoust <42525540+Camezza@users.noreply.github.com>
2021-05-25 12:10:25 +1000

    Add white rectangle

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-pygame.display.set_mode((400, 300)) # Initialise window
-pygame.display.set_caption('Hello World!') # Set the window title
+
+DISPLAY = pygame.display
+
+window = DISPLAY.set_mode((400, 300)) # Initialise window
+DISPLAY.set_caption('Hello World!') # Set the window title
 
 while True: # main game loop
     for event in pygame.event.get(): # Retreive all queued events. Must be ran to display a window
-        if event == KEYDOWN:
-            pygame.display.toggle_fullscreen()
+        pass
+    # draw simple surface
+    surface = pygame.Surface((0, 0))
+    rectangle = pygame.Rect((0, 0), (200, 200))
+    pygame.draw.rect(window, (255, 255, 255), rectangle)
+
     pygame.display.update() # Update the window displayed
